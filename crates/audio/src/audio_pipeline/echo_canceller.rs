3702f6f5cd669303f8d9aa9d5fbfa7a617f04822
solos <lxl1217@gmail.com>
2026-07-27 14:44:01 +0800

    add pages
    add pages

--- crates/audio/src/audio_pipeline/echo_canceller.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Clone, Default)]
-pub struct EchoCanceller;
-
-impl EchoCanceller {
-    pub fn process_reverse_stream(&mut self, _buf: &mut [i16]) {}
-    pub fn process_stream(&mut self, _buf: &mut [i16]) -> anyhow::Result<()> {
-        Ok(())
-    }
-}
